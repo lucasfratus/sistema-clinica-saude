0fccde2187224569c233bc415c510125d4de7a7f
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2024-06-28 20:59:41 -0300

    Medico.java added

--- src/clinica/Medico.java
+++ src/clinica/Medico.java
@@ -9,5 +9,20 @@ package clinica;
  * @author lucas
  */
 public class Medico {
+    private String nome;
+    private String numero_de_registro;
+    private String especialidade;
+    private int numero_consultorio;
+    private String numero_telefone;
+    
+    public Medico() {
+        
+    }
+    
+    public Medico(String nome, String numero_de_registro, String especialidade, int numero_consultorio, String numero_telefone) {
+        this.nome = nome;
+        this.numero_de_registro = numero_de_registro
+                
+    }
     
 }
