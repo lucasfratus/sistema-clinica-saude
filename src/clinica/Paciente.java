06b06a25b6e96c0d2d28428cdefdc33b2145d118
Giovani Oliveira <60044063+imGiovani@users.noreply.github.com>
2024-06-28 20:55:21 -0300

    fmgay

--- src/clinica/Paciente.java
+++ src/clinica/Paciente.java
@@ -9,5 +9,69 @@ package clinica;
  * @author lucas
  */
 public class Paciente {
+    private String nome;
+    private String email;
+    private String dataNascimento;
+    private String endereco;
+    private int telefone;
+    private String convenio;
     
+    public Paciente(){        
+    }
+    public Paciente(String nome, String email, String dataNascimento, String endereco, int telefone, String convenio){
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.convenio = convenio;
+        }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getConvenio() {
+        return convenio;
+    }
+
+    public void setConvenio(String convenio) {
+        this.convenio = convenio;
+    }
 }
