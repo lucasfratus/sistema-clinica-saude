9fd21960abe815b1e20ce626c1115a6bf576fe7d
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2024-06-28 23:35:55 -0300

    Create GerenciadorDeMensagens.java

---
+++ src/clinica/GerenciadorDeMensagens.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinica;
+
+/**
+ *
+ * @author lucas
+ */
+public class GerenciadorDeMensagens {
+    private Paciente paciente;
+    private Consultas consulta;
+    private String Mensagem;
+}
