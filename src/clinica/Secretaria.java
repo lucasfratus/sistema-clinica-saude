985dadfccce762427fe85fad9d8957fb19257b37
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2024-06-28 21:50:47 -0300

    Update Secretaria.java

--- src/clinica/Secretaria.java
+++ src/clinica/Secretaria.java
@@ -9,5 +9,49 @@ package clinica;
  * @author lucas
  */
 public class Secretaria {
+    private String nomeFuncionario;
+    private String horarioAtendimento;
+    private String numeroSecretaria;
+    private Paciente paciente;
+    private Consultas consulta;
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getHorarioAtendimento() {
+        return horarioAtendimento;
+    }
+
+    public void setHorarioAtendimento(String horarioAtendimento) {
+        this.horarioAtendimento = horarioAtendimento;
+    }
+
+    public String getNumeroSecretaria() {
+        return numeroSecretaria;
+    }
+
+    public void setNumeroSecretaria(String numeroSecretaria) {
+        this.numeroSecretaria = numeroSecretaria;
+    }
     
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+    
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    
+    public void setConsulta(Consultas consulta) {
+        this.consulta = consulta;
+    }
+    
+    
+   
 }
+    
