1c44c4bf75e0fcb2018afacd3bb17ce49499c9d7
Giovani Oliveira <60044063+imGiovani@users.noreply.github.com>
2024-06-28 23:13:31 -0300

    relatoriosMedicos

---
+++ src/clinica/RelatoriosMedicos.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinica;
+
+/**
+ *
+ * @author Giovani
+ */
+public class RelatoriosMedicos {
+    private Paciente paciente;
+    private String receita;
+    private boolean atestado;
+    private boolean declaracaoAcompanhamento;
+
+    public RelatoriosMedicos(){
+    }
+    public RelatoriosMedicos(Paciente paciente, String receita, boolean atestado, boolean declaracaoAcompanhamento){
+        this.paciente = paciente;
+        this.receita = receita;
+        this.atestado = atestado;
+        this.declaracaoAcompanhamento = declaracaoAcompanhamento;
+    }
+
+    
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getReceita() {
+        return receita;
+    }
+
+    public void setReceita(String receita) {
+        this.receita = receita;
+    }
+
+    public boolean isAtestado() {
+        return atestado;
+    }
+
+    public void setAtestado(boolean atestado) {
+        this.atestado = atestado;
+    }
+
+    public boolean isDeclaracaoAcompanhamento() {
+        return declaracaoAcompanhamento;
+    }
+
+    public void setDeclaracaoAcompanhamento(boolean declaracaoAcompanhamento) {
+        this.declaracaoAcompanhamento = declaracaoAcompanhamento;
+    }
+    
+    
+}
