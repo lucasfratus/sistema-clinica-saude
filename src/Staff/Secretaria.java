67f9f934cb98251ead3b6ec41f3c9b05c58ac553
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2024-07-20 21:24:07 -0300

    New Packages

--- src/Staff/Secretaria.java
+++ src/Staff/Secretaria.java
@@ -2,7 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package clinica;
+package Staff;
+
+import Atendimento.Consultas;
+import Atendimento.Paciente;
 
 /**
  *
@@ -10,8 +13,6 @@ package clinica;
  */
 public class Secretaria {
     private String nomeFuncionario;
-    private String horarioAtendimento;
-    private String numeroSecretaria;
     private Paciente paciente;
     private Consultas consulta;
 
@@ -21,22 +22,6 @@ public class Secretaria {
 
     public void setNomeFuncionario(String nomeFuncionario) {
         this.nomeFuncionario = nomeFuncionario;
-    }
-
-    public String getHorarioAtendimento() {
-        return horarioAtendimento;
-    }
-
-    public void setHorarioAtendimento(String horarioAtendimento) {
-        this.horarioAtendimento = horarioAtendimento;
-    }
-
-    public String getNumeroSecretaria() {
-        return numeroSecretaria;
-    }
-
-    public void setNumeroSecretaria(String numeroSecretaria) {
-        this.numeroSecretaria = numeroSecretaria;
     }
     
     public void setPaciente(Paciente paciente) {
