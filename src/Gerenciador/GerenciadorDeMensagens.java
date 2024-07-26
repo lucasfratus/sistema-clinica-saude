3d37d021d4a177be2291a84679efe177dd62fde4
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2024-07-26 17:46:18 -0300

    Update GerenciadorDeMensagens.java

--- src/Gerenciador/GerenciadorDeMensagens.java
+++ src/Gerenciador/GerenciadorDeMensagens.java
@@ -14,6 +14,7 @@ import Atendimento.Paciente;
  */
 public class GerenciadorDeMensagens {
     private Secretaria secretaria;
+    private ArrayList<String> mensagensEnviadas;
     
     public ArrayList<Consulta> getPacientesComContato(String dataDeAmanha){
         ArrayList[] relatorio = new ArrayList[2];
@@ -27,18 +28,33 @@ public class GerenciadorDeMensagens {
     public void mandarSms(String mensagem, String telefone){
     }
     
-    public void enviarMensagens(String dataDeAmanha){
+    public String enviarMensagens(String dataDeAmanha){
         ArrayList<Consulta> listaConsultas = new ArrayList();
         listaConsultas = getPacientesComContato(dataDeAmanha);
+       
         for(int i = 0; i < listaConsultas.size(); i++){
             Paciente paciente = listaConsultas.get(i).getPaciente();
             Consulta consulta = listaConsultas.get(i);
             if ("Nao Informado".equals(paciente.getEmail())) {
-                String mensagem = "Prezado(a) " + paciente.getNome() + " A sua consulta com o(a) Doutor(a) " + consulta.getMedico().getNome() + " é amanhã, as"
+                String mensagem = "Prezado(a) " + paciente.getNome() + " A sua consulta com o(a) Doutor(a) " + consulta.getMedico().getNome() + " eh amanha, as"
                         + " " + consulta.getHorario() ;
                 mandarSms(mensagem, paciente.getTelefone());
+                mensagensEnviadas.add("Enviado ao numero: " + paciente.getTelefone() + "\n" + mensagem);
+            } else {
+                String mensagem = "Prezado(a) " + paciente.getNome() + " A sua consulta com o(a) Doutor(a) " + consulta.getMedico().getNome() + " eh amanha, as"
+                        + " " + consulta.getHorario() ;
+                mandarEmail(mensagem, paciente.getEmail());
+                mensagensEnviadas.add("Enviado ao email: " + paciente.getEmail() + "\n" + mensagem);
             }
         }
+        return "Mensagens enviadas com sucesso";   
     }
     
+    public String visualizarMensagensEnviadas(){
+        String buffer = null;
+        for(int x = 0; x < mensagensEnviadas.size(); x++){
+            buffer = buffer + '\n' + mensagensEnviadas.get(x);
+        }
+        return buffer;
+    }
 }
